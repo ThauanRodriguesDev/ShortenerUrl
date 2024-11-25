6a5bd4408fb3254d0c032374b64f2776e3e0e7cc
Taylor <139730493+TaylorSzu@users.noreply.github.com>
2024-11-25 01:24:06 -0300

    Revert "feat: saveUrl in controller"

--- src/main/java/com/project/ShortenerUrl/controller/UrlController.java
+++ src/main/java/com/project/ShortenerUrl/controller/UrlController.java
@@ -1,26 +1,4 @@
 package com.project.ShortenerUrl.controller;
 
-import com.project.ShortenerUrl.models.dto.OriginalUrlDto;
-import com.project.ShortenerUrl.models.dto.ShortUrl;
-import com.project.ShortenerUrl.models.entity.OriginalUrl;
-import com.project.ShortenerUrl.service.UrlService;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-@RestController
-@RequestMapping("/")
 public class UrlController {
-
-    private final UrlService urlService;
-
-    public UrlController(UrlService urlService) {
-        this.urlService = urlService;
-    }
-
-    @PostMapping
-    public ResponseEntity<ShortUrl> saveUrl(@RequestBody OriginalUrlDto url){
-        ShortUrl shortUrl = urlService.saveOriginalUrl(url);
-        return ResponseEntity.status(HttpStatus.CREATED).body(shortUrl);
-    }
 }
