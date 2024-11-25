d42809db7c8c3931f2786f72f096d083ffd45267
Thauan <dev.thauan.rodrigues@gmail.com>
2024-11-24 23:45:20 -0300

    feat: added entities in the project

--- src/main/java/com/project/ShortenerUrl/repository/UrlRepository.java
+++ src/main/java/com/project/ShortenerUrl/repository/UrlRepository.java
@@ -2,7 +2,6 @@ package com.project.ShortenerUrl.repository;
 
 import com.project.ShortenerUrl.models.entity.OriginalUrl;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface UrlRepository extends JpaRepository<OriginalUrl, Long> {
 }
