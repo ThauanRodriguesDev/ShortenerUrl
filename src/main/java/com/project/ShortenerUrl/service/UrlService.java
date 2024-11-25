10fa9779498a0476c36a9dfd054452e5c23ea0d8
TaylorSzu <sherekgostoso3@gmail.com>
2024-11-25 00:17:31 -0300

    feat: sevice add

--- src/main/java/com/project/ShortenerUrl/service/UrlService.java
+++ src/main/java/com/project/ShortenerUrl/service/UrlService.java
@@ -1,6 +1,34 @@
 package com.project.ShortenerUrl.service;
 
+import com.project.ShortenerUrl.models.dto.OriginalUrlDto;
+import com.project.ShortenerUrl.models.dto.ShortUrl;
+import com.project.ShortenerUrl.models.entity.OriginalUrl;
+import com.project.ShortenerUrl.repository.UrlRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
 public class UrlService {
+    private final UrlRepository urlRepository;
 
+    public UrlService(UrlRepository urlRepository) {
+        this.urlRepository = urlRepository;
+    }
 
+    public ShortUrl add(OriginalUrlDto urlDto){
+        UUID uuid = UUID.randomUUID();
+        String shortUrl = uuid.toString().replace("-", "").substring(0,8);
+
+        OriginalUrl url = new OriginalUrl();
+        url.setShortenerUrl(shortUrl);
+        url.setOriginalUrl(urlDto.getOriginalUrl());
+        url.setExpirationTime(urlDto.getExpirationTime());
+
+        urlRepository.save(url);
+
+        ShortUrl shortUrlDto = new ShortUrl(shortUrl);
+
+        return shortUrlDto;
+    }
 }
